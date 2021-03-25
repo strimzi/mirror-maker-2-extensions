f04824db34ec746b95a395e3b49a7513255b3382
Jakub Scholz <www@scholzj.com>
2021-03-25 14:07:58 +0100

    Prepare build to rename master branch to main (#5)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .azure/scripts/build.sh
+++ .azure/scripts/build.sh
@@ -19,11 +19,11 @@ mvn $MVN_ARGS spotbugs:check
 # Push to Nexus
 if [ "$BUILD_REASON" == "PullRequest" ] ; then
     echo "Building Pull Request - nothing to push"
-elif [[ "$BRANCH" != "refs/tags/"* ]] && [ "$BRANCH" != "refs/heads/master" ]; then
-    echo "Not in master branch or in release tag - nothing to push"
+elif [[ "$BRANCH" != "refs/tags/"* ]] && [ "$BRANCH" != "refs/heads/main" ]; then
+    echo "Not in main branch or in release tag - nothing to push"
 else
    if [ "${MAIN_BUILD}" = "TRUE" ] ; then
-       echo "In master branch or in release tag - pushing to nexus"
+       echo "In main branch or in release tag - pushing to nexus"
        ./.azure/scripts/push-to-nexus.sh
    fi
 fi
