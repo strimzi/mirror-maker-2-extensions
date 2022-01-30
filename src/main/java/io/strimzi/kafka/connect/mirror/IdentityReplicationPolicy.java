39ec2597672a91776a5d438dbc11cfa61abc86dc
Jakub Scholz <www@scholzj.com>
2022-01-30 22:42:38 +0100

    Update IdentityReplicationPolicy to extend Kafka's own policy
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- src/main/java/io/strimzi/kafka/connect/mirror/IdentityReplicationPolicy.java
+++ src/main/java/io/strimzi/kafka/connect/mirror/IdentityReplicationPolicy.java
@@ -4,55 +4,12 @@
  */
 package io.strimzi.kafka.connect.mirror;
 
-import java.util.Map;
-import org.apache.kafka.connect.mirror.DefaultReplicationPolicy;
-import org.apache.kafka.connect.mirror.MirrorConnectorConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
- * <p>
- *   This replication policy is supposed to be used for a simple two-site active-passive Kafka DRP setup where mirroring is always enabled
- *   in one direction only and where names of the downstream topics are not prefixed by Mirror Maker 2, so that switching of the active site
- *   1) does not require reconfiguration of the business services (i.e., we do not have to change names of the topics the services use) and
- *   2) does not add one more prefix to the topic name (i.e., we do not have topic names growing each time the switch happens).
- * </p>
- * <p>
- *   This solution seems to work with Mirror Maker version 2.4.0 but it is 'hacky' because it violates spec of the ReplicationPolicy
- *   interface. According to the spec, if <code>upstreamTopic</code> returns null then <code>topicSource</code> should also return null,
- *   but with this implementation it is not the case. We had to make <code>topicSource</code> return alias of the upstream cluster because
- *   otherwise mirroring of topic properties (e.g., "cleanup.policy=compact") did not work.
- * </p>
+ * The IdentityReplicationPolicy is used to replicate topics using Mirror Maker 2 without changing its name. This class
+ * originally implemented such policy. But since Kafka 3.0.0, such policy is part of Kafka itself and Strimzi does not
+ * need any more its own. It is still supported in Strimzi deployments for backwards compatibility reasons, but it now
+ * only inherits from the original Kafka policy.
  */
-
-public class IdentityReplicationPolicy extends DefaultReplicationPolicy {
-    private static final Logger log = LoggerFactory.getLogger(IdentityReplicationPolicy.class);
-
-    private String sourceClusterAlias;
-
-    @Override
-    public void configure(Map<String, ?> props) {
-        super.configure(props);
-        sourceClusterAlias = (String) props.get(MirrorConnectorConfig.SOURCE_CLUSTER_ALIAS);
-        if (sourceClusterAlias == null) {
-            String logMessage = String.format("Property %s not found", MirrorConnectorConfig.SOURCE_CLUSTER_ALIAS);
-            log.error(logMessage);
-            throw new RuntimeException(logMessage);
-        }
-    }
-
-    @Override
-    public String formatRemoteTopic(String sourceClusterAlias, String topic) {
-        return topic;
-    }
-
-    @Override
-    public String topicSource(String topic) {
-        return topic == null ? null : sourceClusterAlias;
-    }
-
-    @Override
-    public String upstreamTopic(String topic) {
-        return topic;
-    }
+public class IdentityReplicationPolicy extends org.apache.kafka.connect.mirror.IdentityReplicationPolicy {
+    // This class just extends the Apache Kafka for backwards compatibility.
 }
